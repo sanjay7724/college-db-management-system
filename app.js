81cff1561121e18a20819f6af9d524ba21ffaa16
sanjay7724 <sanjaydwells@gmail.com>
2023-05-28 22:47:19 +0530

    bug fix

--- app.js
+++ app.js
@@ -55,14 +55,14 @@ app.get('/student-data', (req, res) => {
             break;
         case 'dobbtn':
             console.log('DOB button clicked');
-            query= `SELECT date_of_birth from student where email=?`;
+            query = `SELECT date_of_birth from student where email=?`;
             break;
         default:
             console.log('default')
             res.status(500).json({ error: 'no btn clicked' });
             return;
     }
-    conn.query(query,[username], (error, results) => {
+    conn.query(query, [username], (error, results) => {
         console.log('inside query')
         if (error) {
             console.log(error);
@@ -74,7 +74,7 @@ app.get('/student-data', (req, res) => {
     })
 })
 
-app.get('/teachers-dashboard',(req,res)=>{
+app.get('/teachers-dashboard', (req, res) => {
     const btn = req.query.button;
     console.log(btn)
     let query = ''
@@ -82,14 +82,14 @@ app.get('/teachers-dashboard',(req,res)=>{
     const attnd = req.query.attnd;
     const cgpa = req.query.cgpa;
     console.log(cgpa)
-    switch(btn){
+    switch (btn) {
         case 'get-stud-details':
             console.log('get stud detail button clicked');
             if (!studid) {
                 console.log('no stud id')
                 return res.status(400).json({ message: 'Invalid student ID' });
-              }
-            query = `select * from academics where student_id =${studid};`;//might be vulnerable to sqli
+            }
+            query = `select * from academics natural join student where student_id =${studid};`;//might be vulnerable to sqli
             break;
         case 'updatebtn':
             console.log('inside update button');
@@ -97,31 +97,31 @@ app.get('/teachers-dashboard',(req,res)=>{
             break;
 
         default:
-            res.status(500).json({message:'invalid'})
+            res.status(500).json({ message: 'invalid' })
     }
-    conn.query(query,(error,details)=>{
+    conn.query(query, (error, details) => {
         //console.log(studid)
         console.log('inside query');
         console.log(query)
-        if(error){
+        if (error) {
             console.log(error);
-            return res.status(500).json({message:'some error'})
-        }else{
-            if(details.length === 0){
-                return res.status(404).json({message:'not found'});
+            return res.status(500).json({ message: 'some error' })
+        } else {
+            if (details.length === 0) {
+                return res.status(404).json({ message: 'not found' });
             }
             let data = details[0];
-           return res.json(data);
+            return res.json(data);
         }
     })
 
 })
 
 
-app.get('/admin',(req,res)=>{
+app.get('/admin', (req, res) => {
     const btn = req.query.button;
-    const stuid=req.query.stuid;
-    const email=req.query.stuemail;
+    const stuid = req.query.stuid;
+    const email = req.query.stuemail;
     const name = req.query.stuname;
     const dob = req.query.Dob;
     const dept = req.query.department;
@@ -131,25 +131,33 @@ app.get('/admin',(req,res)=>{
     const staff_name = req.query.name;
     const staff_dept = req.query.dept;
     console.log(email);
-    let query=''
-    switch(btn){
+    let query = ''
+    let attendance = "ab"
+    switch (btn) {
         case 'add_stud':
             console.log('addstud button clicked');
-            query = `insert into student(email,name,date_of_birth,department,mobile) values("${email}","${name}","${dob}","${dept}",${mobile});`//vulnerable to sqli
+            query = `INSERT into academics values(${stuid} , CURRENT_DATE ,"absent" , 0 , "${name}", "${email}");`//vulnerable to sqli
+            console.log(query)
+            conn.query(`insert into student(id,email,name,date_of_birth,department,mobile) values(?,?,?,?,?,?);`, [stuid, email, name, dob,dept,mobile], (error, details) => {
+                if(error){
+                    console.log(error);
+                }
+                console.log("hi");
+            })
             break;
         case 'add_staff':
             console.log('addstaff');
-            query=`insert into teacher(email,name,department) values("${staff_email}","${staff_name}","${staff_dept}");`;//vulnerable to sqli
+            query = `insert into teacher(email,name,department) values("${staff_email}","${staff_name}","${staff_dept}");`;//vulnerable to sqli
             break;
         default:
-            return res.status(500).json({message:'invalid'});
+            return res.status(500).json({ message: 'invalid' });
     }
 
-    conn.query(query,(error,details)=>{
-        if(error){
+    conn.query(query, (error, details) => {
+        if (error) {
             console.log(error);
         }
-        let data=details[0];
+        let data = details[0];
         return res.json(data);
     })
 
